fca651de52eaf5fecd2ce74e47d27b26fc862246
David Adam Justice <david@outthinkgroup.com>
2014-10-01 22:34:38 -0700

    fix double time when start called twice

--- werk.js
+++ werk.js
@@ -5,6 +5,8 @@ module.exports = function(self) {
   self.addEventListener('message', function(ev) {
     if (ev.data === "start") {
 
+      if (timerID) clearInterval(timerID);
+
       timerID = setInterval(function() {
                   self.postMessage("tick");
                 }, interval);
